79e9f5723b9584c63e13fd9da99a327021bd4112
Bradley J Clement <Bradley.J.Clement@jpl.nasa.gov>
2012-09-10 23:31:23 +0000

    needed public on Random class

--- src/gov/nasa/jpl/ae/solver/Random.java
+++ src/gov/nasa/jpl/ae/solver/Random.java
@@ -7,7 +7,7 @@ package gov.nasa.jpl.ae.solver;
  * @author bclement
  *
  */
-final class Random {
+public final class Random {
   private static final long seed = 3;  // the magic number
   public static final java.util.Random global = new java.util.Random( seed );
 }
