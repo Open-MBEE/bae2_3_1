0c1d75af9b10a7d25d6da7dbe89654f9ef7290a7
Bradley J Clement <Bradley.J.Clement@jpl.nasa.gov>
2013-01-12 01:00:35 +0000

    Edited main.py with a function that adds a path to the python path only if the path has not already been added.  I also converted all of the tabs to spaces.

--- src/gov/nasa/jpl/ae/magicdrawPlugin/main.py
+++ src/gov/nasa/jpl/ae/magicdrawPlugin/main.py
@@ -4,7 +4,6 @@ from com.nomagic.magicdraw.ui.actions import DefaultDiagramAction
 from com.nomagic.magicdraw.uml.symbols import DiagramPresentationElement
 from com.nomagic.magicdraw.uml.symbols import PresentationElement
 from com.nomagic.magicdraw.actions import *
-from javax.swing import JOptionPane
 from com.nomagic.actions import *
 from com.nomagic.magicdraw.core import Application
 
@@ -13,8 +12,13 @@ from java.lang import String
 import traceback
 
 import os
-workspacePath = 'C:\\Users\\bclement\\workspaceFresh'
-mdPath = 'C:\\Program Files\\MagicDraw\\IMCE-GENPROF-17.0sp5-build99-20120615'
+from os.path import expanduser
+
+home = expanduser("~")
+
+workspacePath = home + os.sep + 'workspace'
+#mdPath = 'C:\\Program Files\\MagicDraw\\IMCE-GENPROF-17.0sp5-build99-20120615'
+mdPath = 'C:\\Program Files\\MagicDraw\\OpsRevMD1702-20120912'
 
 projectPath = workspacePath + os.sep + 'CS'
 pluginSrcPath = projectPath + os.sep + 'src' + os.sep + 'gov' + os.sep + \
@@ -22,39 +26,44 @@ pluginSrcPath = projectPath + os.sep + 'src' + os.sep + 'gov' + os.sep + \
 aePluginDir = mdPath + os.sep + 'plugins' + os.sep + \
              'com.nomagic.magicdraw.jpython' + os.sep + 'scripts' + os.sep + 'LADWP'
 
-if os.getenv('HOME') == '/Users/mjackson': 
+if home == '/Users/mjackson': 
     workspacePath = '/Users/mjackson/Documents/workspace-Helios/'
     mdpath = '/Applications/OpsRevMD1702-20120818/'
     aePluginDir = mdPath + os.sep + 'plugins' + os.sep + \
              'com.nomagic.magicdraw.jpython' + os.sep + 'scripts' + os.sep + 'magicdrawPlugin'
 import sys
-sys.path.append(aePluginDir)
-sys.path.append(pluginSrcPath);
-sys.path.append(projectPath + os.sep + 'bin');
-sys.path.append(projectPath + os.sep + 'AE.jar');
+def addToPath(s):
+    if not(s in sys.path):
+        sys.path.append(s)
+
+addToPath(aePluginDir)
+addToPath(pluginSrcPath);
+addToPath(projectPath + os.sep + 'bin');
+addToPath(projectPath + os.sep + 'AE.jar');
+#print 'AE sys.path = ' + sys.path
 
 class ExampleAction( MDAction ):
-	def __init__(self, name, s):
-		gl = Application.getInstance().getGUILog()
-		self.s = s
-		MDAction.__init__( self,"", name, None, None )
-		#gl.log(str(s))
+    def __init__(self, name, s):
+        gl = Application.getInstance().getGUILog()
+        self.s = s
+        MDAction.__init__( self,"", name, None, None )
+        #gl.log(str(s))
 
-	def actionPerformed(self, event):
-		gl = Application.getInstance().getGUILog()
-		try:
-			#gl.log("blah")
-			mod = __import__(self.getName().replace(" ", "_"))
-			reload(mod)
-			mod.run(self.s)
-		except:
-			exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-			gl.log("*** EXCEPTION:")
-			messages=traceback.format_exception(exceptionType, exceptionValue, exceptionTraceback)
-			for message in messages:
-				gl.log(message)
-		
-	
+    def actionPerformed(self, event):
+        gl = Application.getInstance().getGUILog()
+        try:
+            #gl.log("blah")
+            mod = __import__(self.getName().replace(" ", "_"))
+            reload(mod)
+            mod.run(self.s)
+        except:
+            exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+            gl.log("*** EXCEPTION:")
+            messages=traceback.format_exception(exceptionType, exceptionValue, exceptionTraceback)
+            for message in messages:
+                gl.log(message)
+        
+    
 # Configurator adds new menu item to First menu in main menu
 class MainMenuConfigurator( AMConfigurator):
     def configure( self,manager ):
@@ -82,89 +91,90 @@ class MainMenuConfigurator( AMConfigurator):
 
 class DiagramConfiguratorIBD(DiagramContextAMConfigurator):
     def configure(self,manager,diagram,selected,requestor):
-    	active = False
-    	try:
-			if active:
-				category=MDActionsCategory("LADWP_Utils","LADWP_Utils")
-				category.setNested(True)
-				#category.addAction(ExampleAction("SpacePorts",selected))
-				#category.addAction(ExampleAction("Align",selected))
-				#category.addAction(ExampleAction("MakeAssociationClassPattern",selected))
-				manager.addCategory(category)
-				#gl.log(str(diagram) + " " + str(selected) + " " + str(requestor))
-			else: pass
+        active = False
+        try:
+            if active:
+                category=MDActionsCategory("LADWP_Utils","LADWP_Utils")
+                category.setNested(True)
+                #category.addAction(ExampleAction("SpacePorts",selected))
+                #category.addAction(ExampleAction("Align",selected))
+                #category.addAction(ExampleAction("MakeAssociationClassPattern",selected))
+                manager.addCategory(category)
+                #gl.log(str(diagram) + " " + str(selected) + " " + str(requestor))
+            else: pass
         except:
-			gl.log("exception")
-			exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-			gl.log("*** EXCEPTION:")
-			messages=traceback.format_exception(exceptionType, exceptionValue, exceptionTraceback)
-			for message in messages:
-				gl.log(message)
+            gl.log("exception")
+            exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+            gl.log("*** EXCEPTION:")
+            messages=traceback.format_exception(exceptionType, exceptionValue, exceptionTraceback)
+            for message in messages:
+                gl.log(message)
     def getPriority(self):
         return AMConfigurator.LOW_PRIORITY
        
-       	
+           
 class DiagramConfigurator(DiagramContextAMConfigurator):
     def configure(self,manager,diagram,selected,requestor):
-        active = False
-    	try:
-			if active:
-				category=MDActionsCategory("LADWP","LADWP")
-				category.setNested(True)
-				#category.addAction(ExampleAction("SpacePorts",selected))
-				#category.addAction(ExampleAction("Align",selected))
-				#category.addAction(ExampleAction("MakeTreeNonNavigable",selected))
-				manager.addCategory(category)
-				#gl.log(str(diagram) + " " + str(selected) + " " + str(requestor))
-			else: pass
+        active = True
+        try:
+            if active:
+                category=MDActionsCategory("LADWP","LADWP")
+                category.setNested(True)
+                #category.addAction(ExampleAction("SpacePorts",selected))
+                #category.addAction(ExampleAction("Align",selected))
+                #category.addAction(ExampleAction("MakeTreeNonNavigable",selected))
+                category.addAction(ExampleAction("whatsMyId",None))
+                manager.addCategory(category)
+                #gl.log(str(diagram) + " " + str(selected) + " " + str(requestor))
+            else: pass
         except:
-			gl.log("exception")
-			exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-			gl.log("*** EXCEPTION:")
-			messages=traceback.format_exception(exceptionType, exceptionValue, exceptionTraceback)
-			for message in messages:
-				gl.log(message)
+            gl.log("exception")
+            exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+            gl.log("*** EXCEPTION:")
+            messages=traceback.format_exception(exceptionType, exceptionValue, exceptionTraceback)
+            for message in messages:
+                gl.log(message)
     def getPriority(self):
         return AMConfigurator.LOW_PRIORITY
 
 class ActDiagramConfigurator(DiagramContextAMConfigurator): 
     def configure(self,manager,diagram,selected,requestor):
         try:
-        	pass
-			#gl.log("Configuring ActDiagramConfigurator!")
-			#category=MDActionsCategory("", "")
-			#category.addAction(ExampleAction("setQueryTargets",selected))
-			#Smanager.addCategory(category)
+            pass
+            #gl.log("Configuring ActDiagramConfigurator!")
+            #category=MDActionsCategory("", "")
+            #category.addAction(ExampleAction("setQueryTargets",selected))
+            #Smanager.addCategory(category)
         except:
-			print "exception"
-			exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-			gl.log("*** EXCEPTION:")
-			messages=traceback.format_exception(exceptionType, exceptionValue, exceptionTraceback)
-			for message in messages:
-				gl.log(message)
+            print "exception"
+            exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+            gl.log("*** EXCEPTION:")
+            messages=traceback.format_exception(exceptionType, exceptionValue, exceptionTraceback)
+            for message in messages:
+                gl.log(message)
     def getPriority(self):
         return AMConfigurator.LOW_PRIORITY
 
 class BrowserContextAMConfigurator(BrowserContextAMConfigurator):
     def configure (self,manager,tree):
         try:
-			category=MDActionsCategory("LADWP","LADWP")
-			category.setNested(True)
-			#category.addAction(ExampleAction("MassRefactor",None))
-			category.addAction(ExampleAction("ExportForAE",None))
-			category.addAction(ExampleAction("ExportForAnalysisEngine_v2",None))
-			category.addAction(ExampleAction("AE",None))
-			#category.addAction(ExampleAction("RegisterAcronyms",None))
-			#category.addAction(ExampleAction("UpdateDocumentNumbering"))
-			#category.addAction(ExampleAction("DocumentViewer"))
-			#category.addAction(ExampleAction("CreateDefaultDocument"))
-			manager.addCategory(category)
+            category=MDActionsCategory("LADWP","LADWP")
+            category.setNested(True)
+            #category.addAction(ExampleAction("MassRefactor",None))
+            category.addAction(ExampleAction("ExportForAE",None))
+            category.addAction(ExampleAction("ExportForAnalysisEngine_v2",None))
+            category.addAction(ExampleAction("AE",None))
+            #category.addAction(ExampleAction("RegisterAcronyms",None))
+            #category.addAction(ExampleAction("UpdateDocumentNumbering"))
+            #category.addAction(ExampleAction("DocumentViewer"))
+            #category.addAction(ExampleAction("CreateDefaultDocument"))
+            manager.addCategory(category)
         except:
-			exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-			gl.log("*** EXCEPTION:")
-			messages=traceback.format_exception(exceptionType, exceptionValue, exceptionTraceback)
-			for message in messages:
-				gl.log(message)
+            exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+            gl.log("*** EXCEPTION:")
+            messages=traceback.format_exception(exceptionType, exceptionValue, exceptionTraceback)
+            for message in messages:
+                gl.log(message)
 
     def getPriority(self):
         return AMConfigurator.LOW_PRIORITY
