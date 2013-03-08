dcd825190696a0a77c0bda9d638ecd71f8a9fbc6
Maddalena Jackson <Maddalena.M.Jackson@jpl.nasa.gov>
2013-03-08 03:31:21 +0000

    some fixes for plotting from file

--- src/gov/nasa/jpl/ae/magicdrawPlugin/PlotDataReader.py
+++ src/gov/nasa/jpl/ae/magicdrawPlugin/PlotDataReader.py
@@ -3,6 +3,7 @@ import InterpolatedMap
 #import SortedDict
 import os
 
+liveUpdating = False
 debug = False
 applicationsDir = "C:\Program Files" 
 homeDir = os.getenv('HOME')
@@ -45,9 +46,11 @@ class PlotDataReader(object):
     reads plottable values from AE output
     '''
 
-    def __init__(self, fileName):
+    def __init__(self, fileName, liveUpdatingI):
         self.fileName = fileName
         self.data = {}
+        global liveUpdating
+        liveUpdating = liveUpdatingI
         self.read(fileName)
         
 #    def __init__(self):
@@ -70,31 +73,10 @@ class PlotDataReader(object):
                 continue
             if not readingExecution:
                 continue
-#            x = re.search("^([A-Za-z0-9_-]*=)?plottable ([^ {]*) ",line)
-            x = re.search("^plottable( projected)? ([^ {]*)( STEP|RAMP|LINEAR)?( [^ {]*)?(.*)",line)
-            if x:
-                debugPrint("x.groups()=" + str(x.groups()))
-                projected = x.groups()[0]
-                if projected != None:
-                    projected = projected.strip()
-                category = x.groups()[1]
-                if "__" in category: category = category.split("__")[-1]
-                if re.search("\d{5}",category): category = category[0:len(category)-6]
-                interpolationType = x.groups()[2]
-                name = x.groups()[3]
-                if name != None:
-                    name = name.strip()
-                if (name == None or len(name) == 0) and interpolationType not in ["STEP", "RAMP", "LINEAR"]:
-                    name = interpolationType
-                    interpolationType = "STEP"
-                mapString = x.groups()[4]
-                m = PlotDataReader.parseMap(mapString)
-                m.interpolationType = interpolationType
-                m.attributes["projected"] = (str(projected).lower() == "projected")
-#                tvm = TimeVaryingPlottableMap("")
-#                tvm.fromString(line, None)
-                if category not in self.data.keys():
-                    self.data[category] = {}
+            
+            (category,name,m) = PlotDataReader.parseMapLine(line)
+            if m:
+                if category not in self.data.keys(): self.data[category] = {}
                 else:
                     originalName = str(name)
                     ctr = 0
@@ -111,11 +93,14 @@ class PlotDataReader(object):
         print( "PlotDataReader: finished loading plottables from " + str(fileName) )
 
     @staticmethod
+   # def parseMapWith(s, prefix0, delimiter0, suffix0, prefix1, delimiter1, suffix1, prefix2, delimiter2, suffix2):
     def parseMapWith(s, prefix1, delimiter1, suffix1, prefix2, delimiter2, suffix2):
         zmap = InterpolatedMap.InterpolatedMap()
         #zmap = SortedDict.SortedDict()
+        #outerOuterPatternStrings = [prefix0, delimiter0, suffix0]
         outerPatternStrings = [prefix1, delimiter1, suffix1]
         innerPatternStrings = [prefix2, delimiter2, suffix2]
+        #outerOuterPatterns = [re.compile(ps) for ps in outerOuterPatternStrings]
         outerPatterns = [re.compile(ps) for ps in outerPatternStrings]
         innerPatterns = [re.compile(ps) for ps in innerPatternStrings]
         
@@ -170,7 +155,11 @@ class PlotDataReader(object):
             try:
                 zmap[int(key)] = float(value)
             except:
-                print("Error! Failed to add key-value to plot: (" + str(key) + "=" + str(value) + ")")
+                try:
+                    (name,category,innerMap) = PlotDataReader.parseMapLine(value,True)
+                    if liveUpdating: zmap[int(key)] = innerMap
+                    else: zmap = innerMap
+                except: PlotDataReader.printStackTrace("Error! Failed to add key-value to plot: (" + str(key) + "=" + str(value) + ")")
             
             #skip over the delimiter
             m = outerPatterns[1].search(s, pos)
@@ -186,17 +175,60 @@ class PlotDataReader(object):
         #zmap.sorted()
         debugPrint("parsed map = " + str(zmap))
         return zmap
-        
+    
+    @staticmethod
+    def parseMapLine(line,plotOptional=False):
+    #   x = re.search("^([A-Za-z0-9_-]*=)?plottable ([^ {]*) ",line)
+        x = re.search("^(plottable )?(projected )?([^ {]*)( STEP|RAMP|LINEAR)?( [^ {]*)?(.*)",line)
+        #else: x = re.search("^plottable( projected)? ([^ {]*)( STEP|RAMP|LINEAR)?( [^ {]*)?(.*)",line)
+        category = name = m = None
+        if x:
+            debugPrint("x.groups()=" + str(x.groups()))
+            plottable = x.groups()[0]
+            if plottable != None: plottable = plottable.strip()
+            else: 
+                if not plotOptional: return (None,None,None)
+            projected = x.groups()[1]
+            if projected != None: projected = projected.strip()
+            category = x.groups()[2]
+            if "__" in category: category = category.split("__")[-1]
+            if re.search("\d{5}",category): category = category[0:len(category)-6]
+            interpolationType = x.groups()[3]
+            name = x.groups()[4]
+            if name != None:
+                name = name.strip()
+            if (name == None or len(name) == 0) and interpolationType not in ["STEP", "RAMP", "LINEAR"]:
+                name = interpolationType
+                interpolationType = "STEP"
+            mapString = x.groups()[5]
+            m = PlotDataReader.parseMap(mapString)
+            m.interpolationType = interpolationType
+            m.attributes["projected"] = (str(projected).lower() == "projected")
+        return (category,name,m)
+    
+     
     @staticmethod
     def parseMap(s):
+        #p = True if s.split("{")[0].contains("projected") else False
+        #p = re.search("{[^{}]*{",s) #if p, it's a projection
         #numRegEx = "\d+(?:\.\d*)?"
         identifierRegEx = "[A-Za-z_]\\w*"
         valuePrefix = "(?:" + identifierRegEx + "(?::(?:" + identifierRegEx + ")?)?\\s*=)?"
         prefix1, delimiter1, suffix1 = "[\\[{(]\\s*", ",\\s*", "\\s*[\\]})]"
         prefix2, delimiter2, suffix2 = "[\\[{(]\\s*" + valuePrefix, "\\s*=\\s*" + valuePrefix, "\\s*[\\]})]"
+        prefix0, delimiter0, suffix0 = "[\\[{(]\\s*", ",\\s*", "\\s*[\\]})]"
         #key, value = numRegEx, numRegEx
+        #return PlotDataReader.parseMapWith(s, prefix0, delimiter0, suffix0, prefix1, delimiter1, suffix1, prefix2, delimiter2, suffix2)
         return PlotDataReader.parseMapWith(s, prefix1, delimiter1, suffix1, prefix2, delimiter2, suffix2)
     
+    @staticmethod
+    def printStackTrace(msg=None):
+        import traceback
+        exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+        messages=traceback.format_exception(exceptionType, exceptionValue, exceptionTraceback)
+        for message in messages:
+            print message
+        if msg: print msg
 #
 # Main test
 #
