31d851a5eb831157478d5b50fb2d49cb9174781c
bclement <Bradley.J.Clement@jpl.nasa.gov>
2013-12-16 08:04:25 -0800

    fixing bug where effect variables didn't match; other small fixes

--- src/gov/nasa/jpl/ae/event/EventSimulation.java
+++ src/gov/nasa/jpl/ae/event/EventSimulation.java
@@ -46,8 +46,8 @@ public class EventSimulation extends java.util.TreeMap< Integer, Set< Pair< Obje
   //private static final String enthoughtPython = "/Library/Frameworks/Python.framework/Versions/7.3/bin/Python";
   //private static final String enthoughtPythonPath = "/usr/local/epd_free-7.3-2-rh5-x86_64/";
   //private static final String enthoughtPython = "/usr/local/epd_free-7.3-2-rh5-x86_64/bin/python";
-  private static final String enthoughtPythonPath = "c:\\Users\\bclement\\git\\bae\\src\\gov\\nasa\\jpl\\ae\\magicdrawPlugin;C:\\Program Files\\Enthought\\Canopy\\App\\Lib";
-  private static final String enthoughtPython = "C:\\Program Files\\Enthought\\Canopy\\App\\python.exe";
+  private static final String enthoughtPythonPath = "C:\\Users\\bclement\\git\\bae\\src\\gov\\nasa\\jpl\\ae\\magicdrawPlugin;C:\\Program Files\\Enthought\\Canopy\\App\\appdata\\canopy-1.0.3.1262.win-x86_64\\Lib;C:\\Program Files\\Enthought\\Canopy\\App\\Lib";
+  private static final String enthoughtPython = "C:\\Program Files\\Enthought\\Canopy\\App\\appdata\\canopy-1.0.3.1262.win-x86_64\\python.exe";
   //private static final String enthoughtPython = "c:\\Python27\\python.exe";
   private static final String enthoughtTempDir = "c:\\temp";
   public static double maxSecondsToNextEvent = 43200;
