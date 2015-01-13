bc01b4b8f4d8de30e61ace5fcddff958bf234d0a
bclement <bclement@jpl.nasa.gov>
2015-01-13 12:28:01 -0800

    added generic parameter to HasId

--- src/gov/nasa/jpl/ae/solver/HasIdImpl.java
+++ src/gov/nasa/jpl/ae/solver/HasIdImpl.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package gov.nasa.jpl.ae.solver;
 
@@ -8,27 +8,27 @@ import gov.nasa.jpl.mbee.util.HasId;
 /**
  *
  */
-public class HasIdImpl implements HasId {
+public class HasIdImpl implements HasId<Integer> {
 
   protected static int counter = 0;
-  
+
   protected final int id = getNext();
-  
+
   public static int getNext() {
     return counter++;
   }
-  
+
   /* (non-Javadoc)
    * @see gov.nasa.jpl.ae.solver.HasId#getId()
    */
   @Override
-  public int getId() {
-    return id; 
+  public Integer getId() {
+    return id;
   }
 
   @Override
   public int hashCode() {
     return id;
   }
-  
+
 }
