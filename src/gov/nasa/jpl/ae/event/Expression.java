b75150acaa3b20829cca173515d3dd88b414baee
Bradley J. Clement <bclement@ljpl.nasa.gov>
2015-09-14 02:18:32 -0700

    minor improvements to debug ouput

--- src/gov/nasa/jpl/ae/event/Expression.java
+++ src/gov/nasa/jpl/ae/event/Expression.java
@@ -679,12 +679,14 @@ public class Expression< ResultType > extends HasIdImpl
     case Constructor: // Groundable -- should not get here
 		case None:
 		default:
-			try {
-				throw new IllegalAccessException();
-			} catch (IllegalAccessException e) {
-			  System.err.println( "Error! Expression has invalid type: " + form );
-				e.printStackTrace();
-			}
+      Debug.error(true, false, "Error! isGrounded(): Expression has invalid type: " + form );
+      if ( Debug.isOn() ) { 
+        try {
+          throw new IllegalAccessException();
+        } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        }
+      }
 			return false; // TODO -- REVIEW -- exit?
 		}
 	}
@@ -708,11 +710,14 @@ public class Expression< ResultType > extends HasIdImpl
     case Constructor: // Groundable -- should not get here
 		case None:
 		default:
-			try {
-				throw new IllegalAccessException();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			}
+      Debug.error(true, false, "Error! ground(): Expression has invalid type: " + form );
+      if ( Debug.isOn() ) { 
+        try {
+          throw new IllegalAccessException();
+        } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        }
+      }
 			return false; // TODO -- REVIEW -- exit?
 		}
 		//return grounded;
@@ -794,10 +799,13 @@ public class Expression< ResultType > extends HasIdImpl
       return (Domain< ResultType >)((FunctionCall)expression).getDomain( propagate, seen );
     case None:
     default:
-      try {
-        throw new IllegalAccessException();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
+      Debug.error(true, false, "Error! getDomain(): Expression has invalid type: " + form );
+      if ( Debug.isOn() ) { 
+        try {
+          throw new IllegalAccessException();
+        } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        }
       }
       return null;
     }
