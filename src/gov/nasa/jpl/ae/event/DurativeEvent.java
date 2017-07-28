6f2de315f10715e4a33e3c835597c10a30beceab
Bradley J. Clement <bclement@jpl.nasa.gov>
2017-07-27 20:08:51 -0700

    removed duplicate methods

--- src/gov/nasa/jpl/ae/event/DurativeEvent.java
+++ src/gov/nasa/jpl/ae/event/DurativeEvent.java
@@ -1353,66 +1353,8 @@ public class DurativeEvent extends ParameterListenerImpl implements Event,
     return value;
     
   }
-  
-  
-  
-  public JSONArray kSolutionJSONArr() {
-    JSONArray value = new JSONArray();
-    Parameter< ? > dontNeedParams[] = { startTime, duration, endTime }; 
-    Set< Parameter< ? > > allParams = getParameters( false, null );
-    allParams.removeAll( Arrays.asList( dontNeedParams ) );
-    for ( Parameter< ? > p : allParams ) {
-      JSONObject param = new JSONObject();
-      if ( p.getValueNoPropagate() instanceof ParameterListenerImpl ) {
-        ParameterListenerImpl pLI =
-            ( (ParameterListenerImpl)p.getValueNoPropagate() );
-        param.put( "name", p.getName() );
-        param.put( "type", "class" );
-        JSONArray val = pLI.kSolutionJSONArr();
-        param.put( "value", val );
-        
-      } else {
-        param.put( "name", p.getName() );
-        param.put( "type", "primitive" );
-        param.put( "value", p.getValue().toString()) ;
-      }
-      value.put( param );
-    }
-    return value;
 
-    
-  }
 
-  
-
-  public String kSolutionString( int indent ) {
-
-    StringBuffer sb = new StringBuffer();
-    String indentString = "";
-    for (int i = 0 ; i < indent; i++) {
-      indentString += "   ";
-    }
-    Parameter< ? > dontNeedParams[] = { startTime, duration, endTime }; 
-
-    Set< Parameter< ? > > allParams = getParameters( false, null );
-    allParams.removeAll( Arrays.asList( dontNeedParams ) );
-    for ( Parameter< ? > p : allParams ) {
-      if ( p.getValueNoPropagate() instanceof ParameterListenerImpl ) {
-        ParameterListenerImpl pLI =
-            ( (ParameterListenerImpl)p.getValueNoPropagate() );
-        sb.append( indentString + p.getName() + " = " + pLI.getClass().getSimpleName()
-                   + " {\n" );
-        sb.append(  pLI.kSolutionString( indent + 1 ) );
-        sb.append( "}\n" );
-      } else {
-        sb.append(indentString + p.getName() + " = " + p.getValue().toString() + "\r\n" );
-      }
-    
-
-  }
-
-    return sb.toString();
-  }
 
   /*
    * (non-Javadoc)
